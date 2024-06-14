2dad388a683daf7d006e11c713a8aa1ed4dfba09
Shoishob Al-Baki <84293221+Shoishob007@users.noreply.github.com>
2024-06-14 18:18:39 +0600

    Implemented json-based model

--- backend/database.js
+++ backend/database.js
@@ -1,15 +1,15 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
+// const sqlite3 = require('sqlite3').verbose();
+// const db = new sqlite3.Database(':memory:');
 
-db.serialize(() => {
-    db.run("CREATE TABLE trade_data (id INTEGER PRIMARY KEY AUTOINCREMENT, trade_code TEXT, date TEXT, close REAL, volume INTEGER)");
+// db.serialize(() => {
+//     db.run("CREATE TABLE trade_data (id INTEGER PRIMARY KEY AUTOINCREMENT, trade_code TEXT, date TEXT, close REAL, volume INTEGER)");
 
-    const stmt = db.prepare("INSERT INTO trade_data (trade_code, date, close, volume) VALUES (?, ?, ?, ?)");
-    const data = require('./data.json');
-    data.forEach(row => {
-        stmt.run(row.trade_code, row.date, row.close, row.volume);
-    });
-    stmt.finalize();
-});
+//     const stmt = db.prepare("INSERT INTO trade_data (trade_code, date, close, volume) VALUES (?, ?, ?, ?)");
+//     const data = require('./data.json');
+//     data.forEach(row => {
+//         stmt.run(row.trade_code, row.date, row.close, row.volume);
+//     });
+//     stmt.finalize();
+// });
 
-module.exports = db;
+// module.exports = db;
--- backend/routes/dataRoutes.js
+++ backend/routes/dataRoutes.js
@@ -1,49 +1,67 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../database');
+const fs = require('fs');
+const path = require('path');
+const dataFilePath = path.join(__dirname, '../data.json');
+
+const readData = () => {
+    const rawData = fs.readFileSync(dataFilePath);
+    return JSON.parse(rawData);
+};
+
+const writeData = (data) => {
+    fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
+};
 
 router.get('/data', (req, res, next) => {
-    db.all('SELECT * FROM trade_data', [], (err, rows) => {
-        if (err) {
-            next(err);
-        } else {
-            res.json(rows);
-        }
-    });
+    try {
+        const data = readData();
+        res.json(data);
+    } catch (err) {
+        next(err);
+    }
 });
 
 router.post('/data', (req, res, next) => {
-    const { trade_code, date, close, volume } = req.body;
-    db.run('INSERT INTO trade_data (trade_code, date, close, volume) VALUES (?, ?, ?, ?)', [trade_code, date, close, volume], function (err) {
-        if (err) {
-            next(err);
-        } else {
-            res.json({ id: this.lastID });
-        }
-    });
+    try {
+        const data = readData();
+        const newItem = { id: data.length + 1, ...req.body };
+        data.push(newItem);
+        writeData(data);
+        res.json(newItem);
+    } catch (err) {
+        next(err);
+    }
 });
 
 router.put('/data/:id', (req, res, next) => {
-    const { id } = req.params;
-    const { trade_code, date, close, volume } = req.body;
-    db.run('UPDATE trade_data SET trade_code = ?, date = ?, close = ?, volume = ? WHERE id = ?', [trade_code, date, close, volume, id], function (err) {
-        if (err) {
-            next(err);
-        } else {
+    try {
+        const data = readData();
+        const { id } = req.params;
+        const index = data.findIndex(item => item.id === parseInt(id));
+
+        if (index !== -1) {
+            data[index] = { id: parseInt(id), ...req.body };
+            writeData(data);
             res.json({ message: 'Update successful' });
+        } else {
+            res.status(404).json({ message: 'Item not found' });
         }
-    });
+    } catch (err) {
+        next(err);
+    }
 });
 
 router.delete('/data/:id', (req, res, next) => {
-    const { id } = req.params;
-    db.run('DELETE FROM trade_data WHERE id = ?', [id], function (err) {
-        if (err) {
-            next(err);
-        } else {
-            res.json({ message: 'Delete successful' });
-        }
-    });
+    try {
+        const data = readData();
+        const { id } = req.params;
+        const newData = data.filter(item => item.id !== parseInt(id));
+        writeData(newData);
+        res.json({ message: 'Delete successful' });
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = router;
